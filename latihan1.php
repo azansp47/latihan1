6aacc1effb33d44779c9793f0090b206457b693f
unknown <azansp47@gmail.com>
2020-11-15 18:39:27 +0700

    latihan1

---
+++ latihan1.php
@@ -0,0 +1,15 @@
+<?php
+$strings = "string";
+$data = "data azan";
+$integer = 47;
+$double = 47.5;
+$boolean = true;
+$booleanF= false;
+$array = [$strings,$data,$integer,$double,$boolean,$booleanF];
+$array2 = array($strings,$integer,$double,$boolean,$booleanF);
+// echo $array; / echo untuk menampilkan satuan variable dan stringclear
+//  0 itu false
+//  1 itu true
+// var_dump($array); // untuk melihat type data
+// print_r($array); // untuk menampilkan data
+
